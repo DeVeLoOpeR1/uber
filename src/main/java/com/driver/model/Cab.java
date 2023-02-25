b481f8a8baf1fb4e7ad7ff11932fc6b253bca4a5
saurabh yadav <saurabh.sy2001>
2023-02-25 16:57:57 +0530

    made all the changes

---
+++ src/main/java/com/driver/model/Cab.java
@@ -0,0 +1,60 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cab")
+public class Cab {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cabId;
+    private int perKmRate;
+    private boolean available;
+
+    // mapping with the driver class
+    @OneToOne
+    @JoinColumn
+    private Driver driver;
+
+    public Cab() {
+    }
+
+    public Cab(int cabId, int perKmRate, boolean available, Driver driver) {
+        this.cabId = cabId;
+        this.perKmRate = perKmRate;
+        this.available = available;
+        this.driver = driver;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public int getCabId() {
+        return cabId;
+    }
+
+    public void setCabId(int cabId) {
+        this.cabId = cabId;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -1,18 +1,14 @@
 package com.driver.services.impl;
 
-import com.driver.model.TripBooking;
+import com.driver.model.*;
 import com.driver.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.driver.model.Customer;
-import com.driver.model.Driver;
 import com.driver.repository.CustomerRepository;
 import com.driver.repository.DriverRepository;
 import com.driver.repository.TripBookingRepository;
-import com.driver.model.TripStatus;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,11 +26,14 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void register(Customer customer) {
 		//Save the customer in database
+		customerRepository2.save(customer);
 	}
 
 	@Override
 	public void deleteCustomer(Integer customerId) {
 		// Delete customer without using deleteById function
+		Customer customer = customerRepository2.findById(customerId).get();
+		customerRepository2.delete(customer);
 
 	}
 
@@ -43,17 +42,72 @@ public class CustomerServiceImpl implements CustomerService {
 		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
 		//Avoid using SQL query
 
-	}
+		TripBooking tripBooking = new TripBooking();
+		tripBooking.setFromLocation(fromLocation);
+		tripBooking.setToLocation(toLocation);
+		tripBooking.setDistanceInKm(distanceInKm);
+		tripBooking.setTripStatus(TripStatus.CONFIRMED);
+		Customer customer = customerRepository2.findById(customerId).get();
+		tripBooking.setCustomer(customer);
+		List<Driver> driverList = driverRepository2.findAll();
+		Driver driver = null;
+		boolean avail = false;
+		for(Driver driver1 : driverList){
 
+			Cab cab = driver1.getCab();
+			if(cab.isAvailable()){
+				avail=true;
+				driver = driver1;
+				break;
+			}
+
+		}
+		if(!avail){
+			throw new Exception("No cab available!");
+		}
+		System.out.println(driver +" driver available or not please check it there");
+		//set driver to trip Booking
+		tripBooking.setDriver(driver);
+
+		// set the trip bill amount
+		Cab cab = driver.getCab();
+		int bill = (cab.getPerKmRate()*distanceInKm);
+		tripBooking.setBill(bill);
+
+		// set the cab status to not available
+		cab.setAvailable(false);
+
+
+		// update driver trip list
+		driver.getTripBookingList().add(tripBooking);
+
+
+		//update customer trip list
+		customer.getTripBookingList().add(tripBooking);
+		
+		//customerRepository2.save(customer);
+
+
+		customerRepository2.save(customer);
+		return tripBooking;
+
+	}
 	@Override
 	public void cancelTrip(Integer tripId){
 		//Cancel the trip having given trip Id and update TripBooking attributes accordingly
 
+		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
+		tripBooking.setTripStatus(TripStatus.CANCELED);
+		tripBookingRepository2.save(tripBooking);
 	}
 
 	@Override
 	public void completeTrip(Integer tripId){
 		//Complete the trip having given trip Id and update TripBooking attributes accordingly
+
+		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
+		tripBooking.setTripStatus(TripStatus.COMPLETED);
+		tripBookingRepository2.save(tripBooking);
 
 	}
 }
